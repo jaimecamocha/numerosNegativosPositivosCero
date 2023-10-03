94df25280ad617fadbd3ad2612056f63fd2cdd1e
Jaime Fernandez <jaimefernandezcalvo@alumnos.tuniversformacion.es>
2023-10-03 10:28:54 +0200

    Creamos un programa que identifica números positivos, negativos y cero

---
+++ numeros.js
@@ -0,0 +1,21 @@
+// JAIME FERNÁNDEZ CALVO
+//
+
+var num = prompt("Ingrese un número:");
+
+num = parseFloat(num);
+
+switch (true) {
+    case num > 0:
+        console.log("El número es positivo.");
+        alert("El número es positivo.");
+        break;
+    case num < 0:
+        console.log("El número es negativo.");
+        alert("El número es negativo.");
+        break;
+    default:
+        console.log("El número es cero.");
+        alert("El número es cero.");
+        break;
+}
